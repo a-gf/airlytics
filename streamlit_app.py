cbe882ac559a460531db68d53e2764b63d91921d
Andres Grijalba <114876953+a-gf@users.noreply.github.com>
2025-01-18 16:21:00 -0500

    Primer grafico
    Primer gráfico con número de reseñas por aerolinea

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,40 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.title("Proyecto de comparación de la experiencia en diferentes aerolineas")
-st.markdown("""
-### Las aerolineas tenidas en cuenta son las siguientes:
-- Aerolínea 1
-- Aerolínea 2
-- Aerolinea 3
-""")
+st.sidebar.title("Proyecto de comparación de la experiencia en diferentes aerolineas")
 
-st.markdown("## Esta es una prueba")
+DATA_URL = "data/airlines_reviews.csv"
+
+@st.cache_data
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    return data
+
+data = load_data()
+
+
+lista_aerolineas = data['Airline'].unique()
+
+st.markdown("### Las aerolíneas tenidas en cuenta son las siguientes:")
+for airline in lista_aerolineas:
+    st.markdown(f"- {airline}")
+
+
+st.markdown("### Número de reseñas por aerolinea")
+numero_reviews = data['Airline'].value_counts()
+st.write(numero_reviews)
+
+numero_reviews = numero_reviews.reset_index()
+numero_reviews.columns = ['Airline', 'Reviews']
+numero_reviews = numero_reviews.sort_values('Reviews', ascending=False)
+
+fig = px.bar(numero_reviews, y='Airline', x='Reviews',
+             title="Número de reseñas por aerolínea",
+             labels={'Airline': 'Aerolínea', 'Reviews': 'Número de reseñas'},
+             color='Airline',
+             color_discrete_sequence=px.colors.qualitative.Set2,
+             orientation='h')
+fig.update_layout(showlegend=False)
+st.plotly_chart(fig)
